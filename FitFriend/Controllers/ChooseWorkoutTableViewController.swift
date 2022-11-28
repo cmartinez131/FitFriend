257bb20bab97fe6bc189892b144be789f92b017a
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2022-11-28 14:41:26 -0500

    update chooseworkout screen images

--- FitFriend/Controllers/ChooseWorkoutTableViewController.swift
+++ FitFriend/Controllers/ChooseWorkoutTableViewController.swift
@@ -29,19 +29,19 @@ class ChooseWorkoutTableViewController: UITableViewController {
         //appending workout objects which are created in workoutitem.swift
         //maybe i can create a data file to store all the data and created objects
 
-        let fullBodyWorkout = WorkoutItem(name: "Full Body", description: "Most efficent fullbody workout focusing on compound movements", image: UIImage(named: "BenchPressCartoon.png"))
+        let fullBodyWorkout = WorkoutItem(name: "Full Body", description: "Most efficent fullbody workout focusing on compound movements", image: UIImage(named: "fullbody.png"))
         workouts.append(fullBodyWorkout)
         
-        let lowerBodyWorkout = WorkoutItem(name: "Lower Body", description: "Lower body focus on legs and core", image: UIImage(named: "lowerbody.png"))
+        let lowerBodyWorkout = WorkoutItem(name: "Lower Body", description: "Lower body focus on legs and core", image: UIImage(named: "gobletsquat.png"))
         workouts.append(lowerBodyWorkout)
         
         let upperBodyWorkout = WorkoutItem(name: "Upper Body", description: "Upper body workout focusing on chest, back, shoulders, and core", image: UIImage(named: "upperbody.png"))
         workouts.append(upperBodyWorkout)
         
-        let chestWorkout = WorkoutItem(name: "Chest", description: "20 minute workout focused to build bigger and more defined chest", image: UIImage(named: "BenchPressCartoon.png"))
+        let chestWorkout = WorkoutItem(name: "Chest", description: "20 minute workout focused to build bigger and more defined chest", image: UIImage(named: "pecfly.png"))
         workouts.append(chestWorkout)
         
-        let backWorkout = WorkoutItem(name: "Back", description: "4 exercises to target every part of back and build back strength", image: UIImage(named: "back.png"))
+        let backWorkout = WorkoutItem(name: "Back", description: "4 exercises to target every part of back and build back strength", image: UIImage(named: "backgif.png"))
         workouts.append(backWorkout)
         
         let bicepsWorkout = WorkoutItem(name: "Biceps", description: "Feel the Burn and Build massive Biceps", image: UIImage(named: "bicep.png"))
@@ -50,23 +50,26 @@ class ChooseWorkoutTableViewController: UITableViewController {
         let tricepsWorkout = WorkoutItem(name: "Triceps", description: "Feel the Burn and Build massive arms by building triceps", image: UIImage(named: "triceps.png"))
         workouts.append(tricepsWorkout)
         
+        let gluteWorkout = WorkoutItem(name: "Glutes", description: "Grow your Glutes", image: UIImage(named: "goodmorning.png"))
+        workouts.append(gluteWorkout)
+        
         let cardioWorkout = WorkoutItem(name: "Cardio", description: "Increase stamina and cardiovascular health", image: UIImage(named: "running.png"))
         workouts.append(cardioWorkout)
         
-        let HIITWorkout = WorkoutItem(name: "H.I.I.T", description: "High Intensity Interval Training. Periods of fast spurts of intense exercise followed by rest.", image: UIImage(named: "defaultimage.png"))
-        workouts.append(HIITWorkout)
+        let HIITWorkout = WorkoutItem(name: "H.I.I.T", description: "High Intensity Interval Training. Periods of fast spurts of intense exercise followed by rest.", image: UIImage(named: "hiit.png"))
+        workouts.append(HIITWorkout)//do a bicycle workout
         
-        let coreWorkout = WorkoutItem(name: "Core", description: "Build a strong core. Building a strong foundation is important for every exercise.", image: UIImage(named: "defualtimage.png"))
-        workouts.append(coreWorkout)
+        let coreWorkout = WorkoutItem(name: "Core", description: "Build a strong core. Building a strong foundation is important for every exercise.", image: UIImage(named: "abs.png"))
+        workouts.append(coreWorkout) //bodyweight abs workout
         
-        let baseballWorkout = WorkoutItem(name: "Baseball", description: "Increase explosive power with baseball related movemenets", image: UIImage(named: "baseball.png"))
-        workouts.append(baseballWorkout)
+        let baseballWorkout = WorkoutItem(name: "Baseball", description: "Increase explosive power with baseball related movemenets", image: UIImage(named: "ballthrow.png"))
+        workouts.append(baseballWorkout)//focus on medicine balls
         
-        let soccerWorkout = WorkoutItem(name: "Soccer", description: "Increase leg strength, quickness, and agility", image: UIImage(named: "defaultimage.png"))
-        workouts.append(soccerWorkout)
+        let soccerWorkout = WorkoutItem(name: "Soccer", description: "Increase leg strength, quickness, and agility", image: UIImage(named: "lunge.png"))
+        workouts.append(soccerWorkout)//leg workout
         
-        let basketballWorkout = WorkoutItem(name: "Basketball", description: "Increase vertical by focusing on explosive leg movements", image: UIImage(named: "defaultimage.png"))
-        workouts.append(basketballWorkout)
+        let basketballWorkout = WorkoutItem(name: "Basketball", description: "Increase vertical by focusing on explosive leg movements", image: UIImage(named: "jumpsquat.png"))
+        workouts.append(basketballWorkout)//explosiveness focus on legs and core
         
         
     }
